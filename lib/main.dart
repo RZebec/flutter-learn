b79bb1245b22ae796840098233c5921a1493ee87
Robert Zebec <zebec.robert@gmail.com>
2020-10-01 11:10:50 +0200

    Done first tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,39 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   var questions = [
-    'What\'s your favorite colour?',
-    'What\'s your favorite animal?'
+    {
+      'questionText': 'What\'s your favorite colour?',
+      'answers': [
+        'Black',
+        'Red',
+        'Green',
+        'White',
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        'Rabbit',
+        'Snake',
+        'Elephant',
+        'Lion',
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        'Max',
+        'Max',
+        'Max',
+        'Max',
+      ],
+    },
   ];
 
   _answerQuestion() {
     setState(() {
-      _questionIndex++;
+      _questionIndex =
+          questions.length - 1 != _questionIndex ? _questionIndex + 1 : 0;
     });
 
     print('Answer chosen!');
@@ -36,10 +62,11 @@ class _MyAppState extends State<MyApp> {
           title: Text('My First App'),
         ),
         body: Column(children: <Widget>[
-          Question(questions[_questionIndex]),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
+          Question(questions[_questionIndex]['questionText']),
+          ...(questions[_questionIndex]['answers'] as List<String>)
+              .map((answer) {
+            return Answer(_answerQuestion, answer);
+          }).toList(),
         ]),
       ),
     );
